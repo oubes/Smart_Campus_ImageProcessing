2c6e23b220532827a40225012515e66583bf9212
Kareem Youssry <kareem.youssry567@gmail.com>
2024-01-24 20:08:36 +0200

    Bug Fix when using GPU

--- Detectors/YOLOv8.py
+++ Detectors/YOLOv8.py
@@ -8,7 +8,7 @@ class yolo8_model(face_detector):
         model = YOLO("yolov8n-face.pt")
         results = model(rgb_img)
         conf = results[0].boxes.conf
-        fl = np.array(results[0].boxes.xyxy.int())
+        fl = np.array(results[0].boxes.xyxy.cpu().int())
         face_locations = [box for i, box in enumerate(fl) if conf[i] > threshold]
         detector_name = 'yolo8'
         return face_locations, detector_name, detector_config

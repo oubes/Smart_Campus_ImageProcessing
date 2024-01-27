9babafbf8fdf4f593009938f1d53d79f79fc5e84
oubes <157318256+oubes@users.noreply.github.com>
2024-01-27 03:00:43 +0200

    .

--- main.py
+++ main.py
@@ -3,14 +3,14 @@
 
 # models = ['YOLOv8', 'DLIB', 'CV2', 'MTCNN', 'Retinaface']
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
     
 #     for model in models:
 #         face_locations, faces_count, taken_time = Detect(model)    
 # from Recognizers import DLIB
 # DLIB.fr_dlib_model(detector_name = 'MTCNN').run()
 
-    from tasks import Recognize
-    import vars
-    known_names = Recognize(detector_name = vars.handling_config.detector_name, recognizer_name = vars.handling_config.recognizer_name)
-    print(known_names)
+from tasks import Recognize
+import vars
+known_names = Recognize(detector_name = vars.handling_config.detector_name, recognizer_name = vars.handling_config.recognizer_name)
+print(known_names)

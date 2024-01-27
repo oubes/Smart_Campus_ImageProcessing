ec0427bf5d1c991940fe2da31bed1e5c80233387
oubes <157318256+oubes@users.noreply.github.com>
2024-01-27 22:04:50 +0200

    .

--- main.py
+++ main.py
@@ -6,9 +6,8 @@ while True:
     # Load the config
     from vars import read_json
     config = read_json('config.json')
+    
     # Use the config
     known_names = Recognize(detector_name = config['HandlingConfig']['detectorName'], recognizer_name = config['HandlingConfig']['recognizerName'])
     print(known_names)
 
-    # Sleep for a while before re-reading the file
-    time.sleep(0)  # Sleep for 10 seconds

963b31299592038ae29202bf57304e35dbe64eb1
Kareem Youssry <kareem.youssry567@gmail.com>
2024-01-28 17:32:57 +0200

    Revert "."
    This reverts commit f49a4c95b82bf17b826552dd203e0242b964bb0c.

--- Detectors/CV2.py
+++ Detectors/CV2.py
@@ -1,18 +1,15 @@
 from detection import face_detector
 import cv2 as cv
 import numpy as np
-from vars import *
+from vars import config
 
 class cv2_model(face_detector):
     """A subclass of face_detector that uses the OpenCV library."""
     def detector(self, gray_img: np.ndarray, detector_config: dict, rgb_img: np.ndarray):
         """Detect faces in the grayscale image using the OpenCV library and the detector configuration."""
-        print(config)
         sf = config["DetectorConfig"]["CV2"]["scaleFactor"]
         min_nh = config["DetectorConfig"]["CV2"]["minNeighbors"]
-        minLength = config["DetectorConfig"]["CV2"]["minLength"]
-        minWidth = config["DetectorConfig"]["CV2"]["minWidth"]
-        min_win_size = (minLength, minWidth)
+        min_win_size = tuple(detector_config["CV2"]["minSize"])
         detector_name = 'cv2'
         face_cascade = cv.CascadeClassifier(cv.data.haarcascades + 'haarcascade_frontalface_default.xml')
         detections = face_cascade.detectMultiScale(gray_img, scaleFactor=sf, minNeighbors=min_nh, minSize=min_win_size)

4c3e3d49366b83e1a6a5787bdd3f3376f5090a05
Kareem Youssry <kareem.youssry567@gmail.com>
2024-01-29 22:57:39 +0200

    API changed to take info from body

--- api.py
+++ api.py
@@ -2,46 +2,49 @@ from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
-from vars import *
+from vars import config
 from tasks import Recognize
 
+host_config = config
 detectors = ["DLIB", "CV2", "RetinaFace", "MTCNN", "YOLOv8"]
 recognizers = ["DLIB"]
 
 class Config(BaseModel):
-    # Should contain all <optional> & <required> config for all models
-    # Each model should read the config that it requires only
-    img: str = config["ImgConfig"]["InputImgUrl"]
+    # Should contain all <optional> & <required> host_config for all models
+    # Each model should read the host_config that it requires only
+    img: str
     imgs: list
+    detector: str
+    recognizer: str
     ############
     # Detection
     # CV2
-    cv2_scale: float = config["DetectorConfig"]["CV2"]["scaleFactor"]
-    cv2_min_neighbors: int = config["DetectorConfig"]["CV2"]["minNeighbors"]
-    cv2_min_size: tuple = (config["DetectorConfig"]["CV2"]["minLength"], config["DetectorConfig"]["CV2"]["minWidth"])
+    cv2_scale: float = host_config["DetectorConfig"]["CV2"]["scaleFactor"]
+    cv2_min_neighbors: int = host_config["DetectorConfig"]["CV2"]["minNeighbors"]
+    cv2_min_size: tuple = (host_config["DetectorConfig"]["CV2"]["minLength"], host_config["DetectorConfig"]["CV2"]["minWidth"])
     # DLIB
-    dlib_upsample: int = config["DetectorConfig"]["DLIB"]["upsampling"]
-    dlib_model: str = config["DetectorConfig"]["DLIB"]["model"]
+    dlib_upsample: int = host_config["DetectorConfig"]["DLIB"]["upsampling"]
+    dlib_model: str = host_config["DetectorConfig"]["DLIB"]["model"]
 
     # RetinaFace
-    retinaface_thresh: float = config["DetectorConfig"]["RetinaFace"]["threshold"]
-    retinaface_scale: int = config["DetectorConfig"]["RetinaFace"]["upsampleScale"]
+    retinaface_thresh: float = host_config["DetectorConfig"]["RetinaFace"]["threshold"]
+    retinaface_scale: int = host_config["DetectorConfig"]["RetinaFace"]["upsampleScale"]
 
     # MTCNN
-    mtcnn_min_face_size: int = config["DetectorConfig"]["MTCNN"]["minFaceSize"]
-    mtcnn_thresh: list = config["DetectorConfig"]["MTCNN"]["thresholds"]
-    mtcnn_scale: float = config["DetectorConfig"]["MTCNN"]["scaleFactor"]
+    mtcnn_min_face_size: int = host_config["DetectorConfig"]["MTCNN"]["minFaceSize"]
+    mtcnn_thresh: list = host_config["DetectorConfig"]["MTCNN"]["thresholds"]
+    mtcnn_scale: float = host_config["DetectorConfig"]["MTCNN"]["scaleFactor"]
 
     # YOLOv8
-    yolo_conf_thres: float = config["DetectorConfig"]["YOLOv8"]["confidenceThreshold"]
+    yolo_conf_thres: float = host_config["DetectorConfig"]["YOLOv8"]["confidenceThreshold"]
 
     #############
     # Recognition
     # DLIB
-    dlib_recog_thresh: float = config["RecognizerConfig"]["DLIB"]["threshold"]
-    dlib_recog_resample: int = config["RecognizerConfig"]["DLIB"]["resample"]
-    dlib_recog_model: str = config["RecognizerConfig"]["DLIB"]["encodingModel"]
-    dlib_recog_encoding_update: int = config["RecognizerConfig"]["DLIB"]["encodingUpdate"]
+    dlib_recog_thresh: float = host_config["RecognizerConfig"]["DLIB"]["threshold"]
+    dlib_recog_resample: int = host_config["RecognizerConfig"]["DLIB"]["resample"]
+    dlib_recog_model: str = host_config["RecognizerConfig"]["DLIB"]["encodingModel"]
+    dlib_recog_encoding_update: int = host_config["RecognizerConfig"]["DLIB"]["encodingUpdate"]
 
 app = FastAPI()
 
@@ -51,18 +54,14 @@ def json(code, res):
         content=jsonable_encoder(res)
         )
 
-@app.post("/")
-def bad_req():
-    return json(400,{"error": "BAD_REQUEST", "message": "Unspecified Model"})
-
 @app.get("/")
 def index():
     return "Hello World!"
 
 
-@app.post("/{detector}/{recognizer}")
-def detect(detector: str, recognizer: str, config: Config):
-    if detector not in detectors:
+@app.post("/detect")
+def detect(config: Config):
+    if config.detector not in detectors:
         return json(
             404,
             {
@@ -70,18 +69,17 @@ def detect(detector: str, recognizer: str, config: Config):
                 "message": f"Couldn't find detection model '{detector}'"
             }
         )
-    if recognizer not in recognizers:
+    if config.recognizer not in recognizers:
         return json(
             404,
             {
                 "error": "NOT_FOUND",
-               "message": f"Couldn't find recognition model '{recognizer}'"
+                "message": f"Couldn't find recognition model '{recognizer}'"
             }
         )
 
-
     for (i, def_detector) in enumerate(detectors):
-        if def_detector != detector:
+        if def_detector != config.detector:
             continue
-        res = Recognize(detector_name = detector, recognizer_name = recognizer)
+        res = Recognize(detector_name = config.detector, recognizer_name = config.recognizer)
         return res

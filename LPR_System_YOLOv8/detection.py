c6678a243e388b68095e539d7a64b303eaf8f1d7
Kareem Youssry <kareem.youssry567@gmail.com>
2024-03-07 12:47:52 +0200

    Bug fix: convert tensor to cpu before using it

--- LPR_System_YOLOv8/detection.py
+++ LPR_System_YOLOv8/detection.py
@@ -1,7 +1,9 @@
 from ultralytics import YOLO
 import numpy as np
-coco_model = YOLO('pretrained_models/yolov8s.pt')
-lpd_model = YOLO('pretrained_models/license_plate_detector.pt')
+
+coco_model = YOLO("pretrained_models/yolov8s.pt")
+lpd_model = YOLO("pretrained_models/license_plate_detector.pt")
+
 
 def detect_cars(img: np.ndarray, vehicles: list) -> np.ndarray:
     """Detect cars in the image using the YOLO model.
@@ -13,11 +15,12 @@ def detect_cars(img: np.ndarray, vehicles: list) -> np.ndarray:
     car_boxes (np.ndarray): The list of bounding boxes for cars.
     """
     coco_results = coco_model(img, verbose=False)
-    all_boxes = np.array(coco_results[0].boxes.xyxy)
-    all_labels = np.array(coco_results[0].boxes.cls)
+    all_boxes = np.array(coco_results[0].boxes.xyxy.cpu())
+    all_labels = np.array(coco_results[0].boxes.cls.cpu())
     desired_labels = np.array(vehicles)
     idx = np.where(np.isin(all_labels, desired_labels))[0]
     return all_boxes[idx]
+
 
 def _detect_lp(img: np.ndarray) -> np.ndarray:
     """Detect the license plate in the image using the YOLO model.
@@ -31,10 +34,11 @@ def _detect_lp(img: np.ndarray) -> np.ndarray:
     try:
         lp_results = lpd_model(img, verbose=False)
         lp_box = lp_results[0].boxes.xyxy[0]
-        lp_box = lp_box.detach().numpy().astype(np.int16)
+        lp_box = lp_box.detach().cpu().numpy().astype(np.int16)
         return lp_box
     except IndexError:
-        print('No lp found')
+        print("No lp found")
+
 
 def detect_lps(imgs: list) -> list:
     """Detect the license plates in the images using the YOLO model.
